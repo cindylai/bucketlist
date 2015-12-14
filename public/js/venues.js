856aeaa27cccd504c92969b743458c3ca510dcd7
Cindy Lai <chuanhsl@andrew.cmu.edu>
2015-12-13 20:58:54 -0500

    added local storage to store inputted location

--- public/js/venues.js
+++ public/js/venues.js
@@ -26,6 +26,14 @@ $(function(){
 		})
 });
 
+$(function () {
+  $('[data-toggle="popover"]').popover()
+});
+
+localStorage.location = $("#location").val();
+console.log("localStorage =",localStorage);
+
+
 
 function findVenues(category) {
 
@@ -52,7 +60,11 @@ function findVenues(category) {
 	var CLIENT_SECRET = "MSERKPZH1RYR2MVIIH21HNRMZBKKU1THCHKGL2E2S10QBAAP";
 	
 	//gets what the user entered
-	$("#location").val("");
+	if ($("#location").val()===undefined){
+		localStorage.location;
+	}
+	else{$("#location").val();}
+	// $("#location").val();
 	try {
 	$.ajax({
  		//url:"https://api.foursquare.com/v2/venues/explore?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&v=20151206"+"&near="+location, //+"&intent=browse", //+"&section="+section,
@@ -76,11 +88,11 @@ function displayDetails(response) {
 	console.log("venues=",response.response["venues"]);
 	var venues = response.response["venues"];
 	if (venues === undefined){
-		search = "<p>Your search did not match any location. Please try again.</p>";
-
+		//search = "<p>Your search did not match any location. Please try again.</p>";
+		search = "<div class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span><span class='sr-only'>Error:</span> Your entered location is not valid. Please try again.</div>";
 	}
 	else{
-		search = "Search Results: <table class='table'>";
+		search = "<h4>Search Results: </h4><table class='table'>";
 
 		for (var i=0; i<venues.length;i++){
 			var name = venues[i]["name"];
@@ -92,7 +104,7 @@ function displayDetails(response) {
 			var url = venues[i]["url"];
 
 			search += "<tr>";
-			search += "<td>"+name+"</td><td><button id='add_venue' class='btn btn-default' type='button'><span class='glyphicon glyphicon-plus'></span></button></td";
+			search += "<td>"+"<button id='moreDetails' type='button' class='btn btn-link'>"+name+"</button>"+"</td><td><button id='add_venue' class='btn btn-default' type='button'><span class='glyphicon glyphicon-plus'></span></button></td";
 			search += "</tr><br>";
 	
 		}
@@ -101,9 +113,12 @@ function displayDetails(response) {
 }
 
 
+
+
 $(function(){
 	$("#responseArea").on("click","#add_venue",function(button) {
 		console.log("clicked add venue!");
 		console.log('button=',button)
+		console.log("this=",$(this).parent().parent());
 		})
 });
